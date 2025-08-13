ff3026dae09ad2dfad5ada37594a60dee479c5b1
agustin teran <agustintern@gmail.com>
2025-08-12 22:19:06 -0300

    static should be especify?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "static",
+  vite: {
+    build: {
+      assetsInlineLimit: 0 // Para mejor manejo de assets en Vercel
+    }
+  }
 });
